739383fba4b2db69e4e0265e36a670e4d5a7027d
Satyam Jaiswal <satyam.jaiswal26@gmail.com>
2020-10-09 10:32:14 +0530

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,10 @@ class MyApp extends StatelessWidget{
   @override 
   Widget build (BuildContext context){
     return new MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Calculator App",
       theme: new ThemeData(
-        primarySwatch: Colors.red),
+        primarySwatch: Colors.blue),
       home: new HomePage(),
     );
   }
